ae13502622ee356af47b574b21cc65864d6704ee
xairaven <alexkovalevkpi@gmail.com>
2023-05-26 02:38:07 +0300

    (Service) Feat: Bitmap service. Creating bitmaps from Genbox.Image

---
+++ StatisticsCW/Services/BitmapService.cs
@@ -0,0 +1,24 @@
+﻿using System.Drawing;
+using System.Net.Http;
+using GenboxImage = Genbox.WolframAlpha.Objects.Image;
+
+namespace StatisticsCW.Services;
+
+public static class BitmapService
+{
+    public static Bitmap ByGenboxImage(GenboxImage image)
+    {
+        using var client = new HttpClient();
+        
+        var url = image.Src;
+        var response = client.GetAsync(url).Result;
+
+        response.EnsureSuccessStatusCode();
+
+        var stream = response.Content.ReadAsStream();
+        
+        var bitmap = new Bitmap(stream);
+
+        return bitmap;
+    }
+}
