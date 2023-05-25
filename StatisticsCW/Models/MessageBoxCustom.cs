1d14ed9f5f6d33b4fb7ed80942a439da54091c07
xairaven <alexkovalevkpi@gmail.com>
2023-05-25 19:47:57 +0300

    (Models) MessageBox: Added new class for avoiding boilerplate

---
+++ StatisticsCW/Models/MessageBoxCustom.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows;
+
+namespace StatisticsCW.Models;
+
+public static class MessageBoxCustom
+{
+    public static void Error(string message)
+    {
+        MessageBox.Show(messageBoxText: message,
+            caption: "Error!",
+            button: MessageBoxButton.OK,
+            icon: MessageBoxImage.Error);
+    }
+}
