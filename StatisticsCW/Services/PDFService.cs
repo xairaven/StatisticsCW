d20fc19531c420eb77d1a2ba050ad05f84190a1d
xairaven <alexkovalevkpi@gmail.com>
2023-05-26 02:38:35 +0300

    (Service) Feat: PDFService. Combining images in PDF

---
+++ StatisticsCW/Services/PDFService.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using Aspose.Words;
+
+namespace StatisticsCW.Services;
+
+public static class PDFService
+{
+    public static Document Combine(List<Bitmap> images, ImageFormat format)
+    {
+        var document = new Document();
+        var builder = new DocumentBuilder(document);
+
+        foreach (var image in images)
+        {
+            using var stream = new MemoryStream();
+            image.Save(stream, format);
+            
+            var bytes = stream.ToArray();
+
+            builder.InsertImage(bytes);
+            
+            // Insert a paragraph break to avoid overlapping images.
+            builder.Writeln();
+        }
+
+        return document;
+    }
+}
