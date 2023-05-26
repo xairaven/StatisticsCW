52b34f603803d3a033703b3573145275f6746f2c
xairaven <alexkovalevkpi@gmail.com>
2023-05-26 17:04:54 +0300

    (Services) Feat: StartupService, configures settings and licenses

---
+++ StatisticsCW/Services/StartupService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Windows;
+using QuestPDF.Infrastructure;
+using StatisticsCW.Data;
+using StatisticsCW.Models;
+
+namespace StatisticsCW.Services;
+
+public static class StartupService
+{
+    public static SettingsContext ConfigureSettings(Window main)
+    {
+        SettingsContext context = null!;
+        
+        try
+        {
+            context = new SettingsContext();
+        }
+        catch (Exception e)
+        {
+            MessageBoxCustom.Error(e.Message);
+            main.Close();
+        }
+
+        return context;
+    }
+
+    public static void ConfigureLicenses()
+    {
+        QuestPDF.Settings.License = LicenseType.Community;
+    }
+}
