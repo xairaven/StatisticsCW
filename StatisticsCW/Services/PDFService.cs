4dd17e333056be8d5a179a39936668a6cd2cc492
xairaven <alexkovalevkpi@gmail.com>
2023-05-26 17:06:06 +0300

    (PDFService) Migration: Aspose.Words replaced by Quest.PDF

--- StatisticsCW/Services/PDFService.cs
+++ StatisticsCW/Services/PDFService.cs
@@ -1,14 +1,60 @@
 ﻿using System.Collections.Generic;
 using System.Drawing;
-using System.Drawing.Imaging;
 using System.IO;
-using Aspose.Words;
+//using Aspose.Words;
+using QuestPDF.Fluent;
+using QuestPDF.Helpers;
+using QuestPDF.Infrastructure;
+using ImageFormat = System.Drawing.Imaging.ImageFormat;
 
 namespace StatisticsCW.Services;
 
 public static class PDFService
 {
     public static Document Combine(List<Bitmap> images, ImageFormat format)
+    {
+        const string header = "Theory of Probability and Mathematical Statistics.\nControl Work.";
+        
+        var document = Document.Create(container =>
+        {
+            container.Page(page =>
+            {
+                page.Size(PageSizes.A4);
+                page.PageColor(Colors.White);
+                page.Margin(1, Unit.Centimetre);
+                page.DefaultTextStyle(x => x.FontSize(12));
+
+                page.Content()
+                    .PaddingVertical(1, Unit.Centimetre)
+                    .Column(x =>
+                    {
+                        x.Item().AlignCenter().Text(header).SemiBold().FontSize(16);
+                        x.Spacing(10);
+
+                        foreach (var image in images)
+                        {
+                            using var stream = new MemoryStream();
+                            image.Save(stream, format);
+            
+                            var bytes = stream.ToArray();
+                            
+                            x.Item().Width(image.Width).Height(image.Height).Image(bytes);
+                        }
+                    });
+
+                page.Footer()
+                    .AlignRight()
+                    .Text(x =>
+                    {
+                        x.CurrentPageNumber();
+                    });
+            });
+        });
+
+        return document;
+    }
+
+    /*public static Document Combine(List<Bitmap> images, ImageFormat format)
     {
         var document = new Document();
         var builder = new DocumentBuilder(document);
@@ -27,5 +73,5 @@ public static class PDFService
         }
 
         return document;
-    }
+    }*/
 }
