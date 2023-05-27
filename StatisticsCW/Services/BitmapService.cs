cc0d72def406b7331bb13a89b9e915686518ebf5
xairaven <alexkovalevkpi@gmail.com>
2023-05-28 02:52:52 +0300

    (Services) Feat: Text to Bitmap method in `BitmapService`

--- StatisticsCW/Services/BitmapService.cs
+++ StatisticsCW/Services/BitmapService.cs
@@ -1,4 +1,6 @@
 ﻿using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Text;
 using System.Net.Http;
 using GenboxImage = Genbox.WolframAlpha.Objects.Image;
 
@@ -18,6 +20,31 @@ public static class BitmapService
         var stream = response.Content.ReadAsStream();
         
         var bitmap = new Bitmap(stream);
+
+        return bitmap;
+    }
+
+    public static Bitmap ByText(string text, int size, FontStyle fontStyle)
+    {
+        Bitmap bitmap = new(1, 1);
+        Graphics graphics = Graphics.FromImage(bitmap);
+
+        Font font = new("Arial", size, fontStyle, GraphicsUnit.Pixel);
+
+        var width = (int)graphics.MeasureString(text, font).Width;
+        var height = (int)graphics.MeasureString(text, font).Height;
+
+        bitmap = new Bitmap(bitmap, new Size(width, height));
+        graphics = Graphics.FromImage(bitmap);
+
+        graphics.Clear(Color.White);
+        graphics.SmoothingMode = SmoothingMode.AntiAlias;
+        graphics.TextRenderingHint = TextRenderingHint.AntiAlias;
+
+        graphics.DrawString(text, font, new SolidBrush(Color.Black), 0, 0);
+
+        graphics.Flush();
+        graphics.Dispose();
 
         return bitmap;
     }
