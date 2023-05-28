14444d6f72e9c20e675c07a10c6b58b64f4ab564
xairaven <alexkovalevkpi@gmail.com>
2023-05-28 23:44:26 +0300

    (View) Feat: `AboutWindow`

---
+++ StatisticsCW/View/AboutWindow.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System.Diagnostics;
+using System.Windows;
+using System.Windows.Navigation;
+
+namespace StatisticsCW.View;
+
+public partial class AboutWindow : Window
+{
+    public AboutWindow()
+    {
+        InitializeComponent();
+    }
+
+    private void Hyperlink_RequestNavigate(object sender, RequestNavigateEventArgs e)
+    {
+        Process.Start(new ProcessStartInfo(e.Uri.AbsoluteUri)
+        {
+            UseShellExecute = true
+        });
+        e.Handled = true;
+    }
+}
