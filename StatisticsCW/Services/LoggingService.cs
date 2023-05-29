fc8243d8d3c451335123f007de2522cf26639c11
xairaven <alexkovalevkpi@gmail.com>
2023-05-29 04:25:34 +0300

    (Services) Feat: `LoggerService` for logging loading

---
+++ StatisticsCW/Services/LoggingService.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using StatisticsCW.View;
+
+namespace StatisticsCW.Services;
+
+public class LoggingService
+{
+    private LoadingWindow _window;
+    
+    public LoggingService(LoadingWindow window)
+    {
+        _window = window;
+    }
+
+    public void LogStatus(int progress, string text, TimeSpan ts)
+    {
+        _window.Dispatcher.Invoke((Action)(() =>
+        {
+            _window.UpdateProgress(progress);
+            _window.LogBox.Text += Text(text, ts);
+        }));
+    }
+
+
+    private string Text(string str, TimeSpan ts)
+    {
+        return $"{Time(ts)} {str}\n";
+    }
+    
+    private string Time(TimeSpan ts)
+    {
+        return string.Format("({0:00}.{1:00} s.)",
+            ts.Seconds, ts.Milliseconds / 10);
+    } 
+}
